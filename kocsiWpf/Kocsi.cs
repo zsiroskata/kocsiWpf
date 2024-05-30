fa10cfc662683746d5f26f544e5cba1e4b4810f5
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-05-30 13:51:08 +0200

    kocsics

---
+++ kocsiWpf/Kocsi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kocsiWpf
+{
+	internal class Kocsi
+	{
+//		név
+        public string Nev { get; set; }
+		//tipus
+		public string Tipus { get; set; }
+		//üzemanyag
+        public string Uzemanyag { get; set; }
+        //évjárat
+        public int Ev { get; set; }
+        //kép
+        public string Kep { get; set; }
+    }
+}
