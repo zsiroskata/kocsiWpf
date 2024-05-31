7a1ee623db09f42ad2482c76dfc7f569ed295751
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-05-31 08:33:47 +0200

    bruh

--- kocsiWpf/Kocsi.cs
+++ kocsiWpf/Kocsi.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
 
 namespace kocsiWpf
 {
@@ -18,5 +20,13 @@ namespace kocsiWpf
         public int Ev { get; set; }
         //kép
         public string Kep { get; set; }
-    }
+		public Kocsi(string nev, string tipus, string uzemanyag, int ev, string source)
+		{
+			Nev = nev;
+			Tipus = tipus;
+			Uzemanyag = uzemanyag;
+			Ev = ev;
+			Kep = $@"..\..\..\images\{source}";
+		}
+	}
 }
--- kocsiWpf/MainWindow.xaml.cs
+++ kocsiWpf/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Metrics;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,64 @@ namespace kocsiWpf
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		List<Kocsi> kocsik = new();
 		public MainWindow()
 		{
 			InitializeComponent();
+			var adatok = File.ReadAllLines(@"..\..\..\src\forras.txt");
+			foreach (var item in adatok)
+			{
+				var sor = item.Split("; ");
+				string nev = sor[0];
+				string tip = sor[1];
+				string uz = sor[2];
+				int ev = int.Parse(sor[3]);
+				string kep = sor[4];
+
+				kocsik.Add(new Kocsi(nev, tip, uz, ev, kep));
+			}
+
+			foreach (var item in kocsik)
+			{
+				kocsikLista.Items.Add(item.Nev);
+			}
+
+			kocsikLista.SelectionChanged += change;
+
+
 		}
+		private void change(object sender, SelectionChangedEventArgs e)
+		{
+			if (kocsikLista.SelectedItem != null)
+			{
+				update();
+			}
+		}
+		private void update()
+		{
+            if (kocsikLista.SelectedItem != null)
+            {
+                foreach (var item in kocsik)
+                {
+					NevLabel.Content = item.Nev;
+					//név
+					//tipus
+					//üzemanyag
+					//évjárat
+					//kép
+					TipusLabel.Content = item.Tipus;
+					uzemanyagLabel.Content = item.Uzemanyag;
+					
+				}
+			}
+        }
+			//- - T Ö R L É S - -
+			private void torles_Click(object sender, RoutedEventArgs e)
+		{
+            if (kocsikLista.SelectedItem != null)
+            {
+                kocsikLista.Items.Remove(kocsikLista.SelectedItem);
+            }
+        }
 	}
 }
